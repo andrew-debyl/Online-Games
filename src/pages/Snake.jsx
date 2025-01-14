e3cabb9321fef5963ce89b2d108a5d0f4d292741
Andrew <andrewdebyl@gmail.com>
2025-01-13 23:24:10 -0500

    snake

--- src/pages/Snake.jsx
+++ src/pages/Snake.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import GateState from '../components/Snake/GateState'
+import "../styles/Snake.css"
 
 function Snake() {
   return (
-    <div>Snake</div>
+    <div className='snake__wrapper'>
+      <GateState/>
+    </div>
   )
 }
 
